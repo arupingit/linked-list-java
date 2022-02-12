4196aaedf3695c89bb62156431d44fee2876b06e
Arup Dutta <dutta.arup59@gmail.com>
2022-02-12 15:59:59 +0530

    Solution Problem18

---
+++ src/com/arup/linkedList/chapter3/Problem18.java
@@ -0,0 +1,40 @@
+package com.arup.linkedList.chapter3;
+
+import com.arup.linkedList.chapter3.LinkedList.Node;
+
+public class Problem18 {
+	/**
+	 * Reverse singly linked list recursively
+	 */
+	static LinkedList sampleList;
+	
+	static{
+		sampleList = new LinkedList();
+		sampleList.insertNode(sampleList, 5);
+		sampleList.insertNode(sampleList, 1);
+		sampleList.insertNode(sampleList, 17);
+		sampleList.insertNode(sampleList, 4);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("first list : ");
+		LinkedList.printList(sampleList);
+		System.out.println("\nsecond list : ");
+		reverseList(sampleList.head);
+		LinkedList.printList(sampleList);
+	}
+	
+	public static void reverseList(Node current){
+		if(current==null){
+			return;
+		}
+		Node var = current.next;
+		if(var==null){
+			sampleList.head = current;
+			return;
+		}
+		reverseList(var);
+		var.next = current;
+		current.next = null;
+	}
+}
