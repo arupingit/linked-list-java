0caa611803c532f7902d6887c70e898f125884a9
Arup Dutta <dutta.arup59@gmail.com>
2022-02-12 11:58:32 +0530

    Solution Problem16

---
+++ src/com/arup/linkedList/chapter3/Problem16.java
@@ -0,0 +1,37 @@
+package com.arup.linkedList.chapter3;
+
+import com.arup.linkedList.chapter3.LinkedList.Node;
+
+public class Problem16 {
+	/**
+	 * Insert node in sorted linked list
+	 */
+	static LinkedList sampleList;
+	static{
+		//create circular list
+		sampleList = new LinkedList();
+		Node node1 =  new Node(1);
+		Node node3 =  new Node(3);
+		Node node4 =  new Node(4);
+		Node node5 =  new Node(5);
+		sampleList.insertNode(sampleList, node1);
+		sampleList.insertNode(sampleList, node3);
+		sampleList.insertNode(sampleList, node4);
+		sampleList.insertNode(sampleList, node5);
+	}
+	public static void main(String[] args){
+		Node node2 = new Node(2);
+		Node pointer = sampleList.head;
+		//Empty list 
+		if(pointer==null){
+			sampleList.head = node2;
+		} else {
+			while(pointer.next!=null && !(pointer.data<node2.data && node2.data<=pointer.next.data)){
+				pointer = pointer.next;
+			}
+			node2.next = pointer.next;
+			pointer.next = node2;			
+		}
+		LinkedList.printList(sampleList);
+	}
+}
